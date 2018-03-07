01d1147620b9371c624906766b92a4e226bca3a9
gdcp <gdcp@m26.local>
2018-03-07 15:49:03 +0800

    修改了

--- firsttry/ViewController.swift
+++ firsttry/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       print("又回到最初的起点")
     }
-
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
